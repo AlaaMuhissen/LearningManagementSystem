d2287a8572a4e59f85ab136bcc2f76a5f8fafc1f
Alaa Muhissen <alaa.muhissen1912@gmail.com>
2024-02-01 10:45:36 +0000

    Implement routing endpoint functions for Student entity CRUD operations

--- serverSide/routes/students.routes.js
+++ serverSide/routes/students.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-
+import {getStudentById, queryStudentTable , createNewStudent, updateStudentDetails, deleteStudent} from '../controller/user.js'
 const router = Router();
 
 
-router.get("/getAllStudents" ,);
-router.get("/getStudent/:id", );
-router.post("/addNewStudent",);
-router.put("/updateStudentDetails/:id",)
-router.delete("/deleteStudent/:id" , );
+router.get("/getAllStudents" , queryStudentTable);
+router.get("/getStudent/:id",getStudentById );
+router.post("/addNewStudent", createNewStudent);
+router.put("/updateStudentDetails/:id", updateStudentDetails)
+router.delete("/deleteStudent/:id" , deleteStudent);
 
 export default router;
