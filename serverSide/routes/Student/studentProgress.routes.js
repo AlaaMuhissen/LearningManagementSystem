65e4e457b5dd0232f95247b9438b1c15035a529e
Alaa Muhissen <alaa.muhissen1912@gmail.com>
2024-02-19 08:54:45 +0200

    Add the student progress routes

---
+++ serverSide/routes/Student/studentProgress.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+import { insertStudentProgress ,getFormattedProgressData ,checkAndUpdateProgress, updateLevelProgress} from "../../controller/Student/studentProgress.js";
+const router = Router();
+
+
+router.get("/createStudentProgress/:studentId/:syllabus_id" , insertStudentProgress);
+router.get("/getStudentProgress/:student_id" , getFormattedProgressData);
+router.put("/updateProgress" , checkAndUpdateProgress);
+router.put("/updateLevel" , updateLevelProgress);
+// router.get("/getLevelNum" , getLevelNum);
+// router.get("/getLevelAndQuestionNumForTopic/:syllabusId/:languageName" , getLevelAndQuestionNumForTopic);
+// router.get("/getStudent/:email",getStudentByEmail);
+// router.post("/addNewStudent", createNewStudent);
+// router.put("/updateStudentDetails/:user_id", updateStudent)
+// router.delete("/deleteStudent/:email" , deleteStudent);
+
+export default router;
